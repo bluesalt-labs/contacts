c6b3124330333789f4b1858e432f8a1d79cfed59
Luke Sontrop <luke@bluesaltlabs.com>
2024-12-29 22:47:49 -0800

    Updates vite to allow serving multiple HTML files.
    i.e. converts this to a MPA (Multi-Page App)

---
+++ src/about/about.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+      <h1>Welcome to the about page</h1>
+  </StrictMode>,
+)
