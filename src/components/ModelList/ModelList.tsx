c44a58ac95912cdf34eff6af0cc75e37e0f23880
Luke Sontrop <luke@bluesaltlabs.com>
2025-01-01 21:07:55 -0800

    Adds basic styling and template updates to people list page. Adds demo link to person profile.

--- src/components/ModelList/ModelList.tsx
+++ src/components/ModelList/ModelList.tsx
@@ -19,15 +19,44 @@ function ModelList({ models }) {
 
 export default ModelList
 
+
+const modelListContainerStyle = {
+  textAlign: "left",
+  border: "2px solid #4A006A",
+  background: "rgba(200, 0, 220, 0.25)",
+  padding: "6px 12px",
+  marginTop: "12px",
+  marginBottom: "12px",
+  cursor: "pointer"
+}
+
 function ModelListItem({ model }) {
+  const imageUrl = `${model.avatar}`
+
+  const title = `${model.first_name} ${model.last_name}`
+  const subtitle = `${model.email} | ${model.phone}`
+
+  const goToProfile = () => {
+    window.location.href =`/people/${model.id}/`
+  }
+
 
   return (
-    <>
-      <div style={{ textAlign: "left" }}>
-        <code><pre>
-          { JSON.stringify(model, null, 2) }
-        </pre></code>
+    <div style={modelListContainerStyle} onClick={goToProfile}>
+      <div style={{ display: "flex" }}>
+
       </div>
-    </>
+
+      <img src={imageUrl} alt={`${title} avatar`} />
+      <br />
+      <span>{title}</span>
+      <br />
+      <span>{subtitle}</span>
+
+      {/* <code><pre>
+        { JSON.stringify(model, null, 2) }
+      </pre></code>
+        */}
+    </div>
   )
 }
