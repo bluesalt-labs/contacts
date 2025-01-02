1f17559b6b182083d58dd70e8f83d0ab3738c4b5
Luke Sontrop <luke@bluesaltlabs.com>
2025-01-01 21:16:49 -0800

    Updates react compiler plugin for vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 const root = resolve(__dirname, 'src')
 const outDir = resolve(__dirname, 'dist')
