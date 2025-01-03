b3b50455afb87437ce41920feadf4d3e8564c9b0
Luke Sontrop <luke@bluesaltlabs.com>
2025-01-03 00:17:12 -0800

    Styling update. Adds initial person model class.

--- src/components/ModelList.jsx
+++ src/components/ModelList.jsx
@@ -21,7 +21,7 @@ function ModelListItem({ model }) {
   const subtitle = `${model.email} | ${model.phone}`
 
   return (
-    <div className="transition rounded px-2 py-3 my-3 border-2 border-fuchsia-900 bg-fuchsia-950 hover:shadow hover:shadow-fuchsia-500 hover:bg-fuchsia-900">
+    <div className="transition text-gray-100 rounded px-2 py-3 my-3 border-2 border-fuchsia-900 bg-fuchsia-950 hover:shadow hover:shadow-fuchsia-500 hover:bg-fuchsia-900">
       <Link to={`${model.id}`}>
         <img className="rounded-full" src={imageUrl} alt={`${title} avatar`} />
         <br />
---
+++ src/models/Person.jsx
@@ -0,0 +1,25 @@
+
+
+
+
+class Person {
+  constructor(attributes) {
+    this.id = attributes?.id ?? 0
+    this.avatar = attributes?.avatar ?? null
+    this.first_name = attributes?.id ?? null
+    this.last_name = attributes?.id ?? null
+    this.surname = attributes?.id ?? null
+    this.username = attributes?.id ?? null
+    this.email = attributes?.id ?? null
+    this.phone = attributes?.id ?? null
+    this.created_at = attributes?.created_at ?? null
+    this.updated_at = attributes?.updated_at ?? null
+    this.deleted_at = attributes?.deleted_at ?? null
+
+  }
+
+  //
+
+}
+
+export default Person
